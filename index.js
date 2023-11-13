6f4c0d0032e30cdc3a0ae0094a9b58ec28653df9
komalone <komalmandal1998@gmail.com>
2023-11-13 13:51:37 +0530

    express 1

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const http= require('http');
-const routes= require('./routes');
+// without express framewark-------------------
+//const http= require('http');
 
-const server= http.createServer(routes);
+//const routes= require('./routes');
 //const server= http.createServer(routes.handler);
+//server.listen(3002);
 
-server.listen(3002);
+// with express framework-------------------
+
+const express= require('express');
+
+const app= express();
+
+app.use((req, res, next)=> {
+    console.log("in middleware");
+    next();   // allows to continue to other middleware;
+})
+
+app.use((req, res, next)=> {
+    console.log("in other middleware");
+    res.send('<h1> Hello by express js</h1>')
+})
+
+app.listen(3002);
+
