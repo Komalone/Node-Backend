dd58a1b2d9d87490f5655d54709186ff8dbfcdac
komalone <komalmandal1998@gmail.com>
2023-11-13 16:23:28 +0530

    body pareser in express

--- index.js
+++ index.js
@@ -9,15 +9,21 @@
 
 const express= require('express');
 
+const bodyParser= require('body-parser');
 const app= express();
 
-app.use((req, res, next)=> {
-    console.log("in middleware");
-    next();   // allows to continue to other middleware;
-})
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next)=> {
-    console.log("in other middleware");
+
+app.use('/add-product', (req, res, next)=> {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit"> Add Product</button></form>')
+});
+app.post('/product', (req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next)=> {
     res.send('<h1> Hello by express js</h1>')
 })
 
